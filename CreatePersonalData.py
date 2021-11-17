c613d9b8694a67186b9a860381cb63e32b89159e
zumio <42907343+zumiosan@users.noreply.github.com>
2021-11-17 14:52:59 +0900

    ファイルの読み込み部分を追加

--- CreatePersonalData.py
+++ CreatePersonalData.py
@@ -8,6 +8,20 @@ import pandas as pd
 from packages.files import get_file_paths, get_directory_paths, save_csv
 
 
+def concat_questionnaire_personal_data(file_paths):
+    """
+    個人のアンケートファイルを複数受け取って結合する．
+    :param file_paths:
+    :return: 楽曲番号と印象を結合したもの，楽曲番号と快不快度を結合したもの，楽曲番号と印象，快不快度を結合したもの
+    """
+
+    data_impression = pd.DataFrame()
+    data_pleasure = pd.DataFrame()
+    data_all = pd.DataFrame()
+    for file_path in file_paths:
+        data =
+
+
 def main():
     # ディレクトリ名取得
     base_path = f".{os.sep}data{os.sep}questionnaire_personal_data/"
@@ -17,9 +31,12 @@ def main():
     personal_impression_data = pd.DataFrame()
     personal_pleasure_data = pd.DataFrame()
     personal_data = pd.DataFrame()
+    for directory_path in directory_paths:
+        file_paths = get_file_paths(directory_path)
+        # print(file_paths)
 
-
-
+        # ファイルを読み込んで結合する
+        data_class, data_pleasure = concat_questionnaire_personal_data(file_paths)
 
 
 
