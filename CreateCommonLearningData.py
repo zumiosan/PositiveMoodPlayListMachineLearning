9e6d2f04c8ca07f510123e44ab5e8cf7905eb757
zumio <42907343+zumiosan@users.noreply.github.com>
2021-12-03 16:06:23 +0900

    共通用学習データを作成するプログラムを作成

--- CreateCommonLearningData.py
+++ CreateCommonLearningData.py
@@ -4,21 +4,25 @@
 import os
 import sys
 import pandas as pd
+from packages.files import save_csv
+from packages.CalcPleasureClass import calc_pleasure_class
 
 
 def main():
     # データ読み込み
     common_data = pd.read_csv('./data/common_data/common_data.csv')
     common_params = pd.read_csv('./data/common_data/common_params.csv')
+    features_data = pd.read_csv('./data/features_data/questionnaire_features_data.csv')
 
-    # 閾値以上なら1（快），未満なら0（不快）
-    for index, row in common_data.iterrows():
-        tmp = common_params[common_params['class'] == row['class']]
-        if row['pleasure'] >= float(tmp['threshold']):
-            common_data.at[index, 'pleasure'] = 1
-        else:
-            common_data.at[index, 'pleasure'] = 0
-    print(common_data)
+    # 閾値以上か未満かでクラス分け
+    common_data = calc_pleasure_class(common_data, common_params)
+    # print(common_data)
+
+    # 特徴量データと結合させる
+    learning_data = pd.merge(common_data, features_data, on='mid')
+
+    # csvを保存
+    save_csv('./data/common_data/learning_data_common.csv', learning_data)
 
 
 if __name__ == '__main__':
