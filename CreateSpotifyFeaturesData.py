01661b9e8d0ed6e195eaf25316baeafe5224910d
zumio <42907343+zumiosan@users.noreply.github.com>
2021-12-18 00:08:07 +0900

    前回のデータから不足分を抽出

--- CreateSpotifyFeaturesData.py
+++ CreateSpotifyFeaturesData.py
@@ -22,6 +22,19 @@ def main():
     all_features_data = pd.merge(all_features_data, spotify_features_data, on='mid')
     # print(all_features_data)
 
+    # 前のデータにはあるのに今回のにはないものを引っ張ってくる．
+    all_features_data_spotify_pre = pd.read_csv('./data/features_data/all_features_data_spotify_pre.csv')
+    del all_features_data_spotify_pre['class']
+    indexs = all_features_data['mid'].values
+    is_not_exist_mid = []
+    for i in all_features_data_spotify_pre['mid'].values:
+        if i not in indexs:
+            print(i)
+            is_not_exist_mid.append(i)
+    all_features_data_spotify_pre = all_features_data_spotify_pre[all_features_data_spotify_pre['mid'].isin(is_not_exist_mid)]
+    all_features_data = pd.concat([all_features_data, all_features_data_spotify_pre]).sort_values(
+        by='mid')
+
     # データを保存
     all_features_data.to_csv('./data/features_data/all_features_data_spotify.csv', encoding='utf-8-sig', index=False)
 
